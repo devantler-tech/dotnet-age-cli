69fc2ad42eb23bf89679fbefed5114aecbb3e984
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-10-17 06:36:47 +0200

    Refactor get-age-binary.sh to support downloading archives in both tar and zip formats

--- .github/scripts/get-age-binary.sh
+++ .github/scripts/get-age-binary.sh
@@ -6,21 +6,26 @@ get() {
   local binary=$2
   local target_dir=$3
   local target_name=$4
-  local isTar=$5
+  local archiveType=$5
 
   echo "Downloading $target_name from $url"
-  if [ "$isTar" = true ]; then
+  if [ "$archiveType" = "tar" ]; then
     curl -LJ "$url" | tar xvz -C "$target_dir" "$binary"
     mv "$target_dir/$binary" "${target_dir}/$target_name"
-  elif [ "$isTar" = false ]; then
+  elif [ "$archiveType" = "zip" ]; then
+    curl -LJ "$url" -o "$target_dir/$target_name.zip"
+    unzip "$target_dir/$target_name.zip" -d "$target_dir"
+    mv "$target_dir/$binary" "${target_dir}/$target_name"
+    rm "$target_dir/$target_name.zip"
+  elif [ "$archiveType" = false ]; then
     curl -LJ "$url" -o "$target_dir/$target_name"
   fi
   chmod +x "$target_dir/$target_name"
 }
 
-get "https://getbin.io/FiloSottile/age?os=darwin&arch=amd64" "age/age-keygen" "Devantler.AgeCLI/runtimes/osx-x64/native" "age-keygen-osx-x64" true
-get "https://getbin.io/FiloSottile/age?os=darwin&arch=arm64" "age/age-keygen" "Devantler.AgeCLI/runtimes/osx-arm64/native" "age-keygen-osx-arm64" true
-get "https://getbin.io/FiloSottile/age?os=linux&arch=amd64" "age/age-keygen" "Devantler.AgeCLI/runtimes/linux-x64/native" "age-keygen-linux-x64" true
-get "https://getbin.io/FiloSottile/age?os=linux&arch=arm64" "age/age-keygen" "Devantler.AgeCLI/runtimes/linux-arm64/native" "age-keygen-linux-arm64" true
-get "https://getbin.io/FiloSottile/age?os=windows&arch=amd64" "age/age-keygen.exe" "Devantler.AgeCLI/runtimes/win-x64/native" "age-keygen-win-x64.exe" true
+get "https://getbin.io/FiloSottile/age?os=darwin&arch=amd64" "age/age-keygen" "Devantler.AgeCLI/runtimes/osx-x64/native" "age-keygen-osx-x64" "tar"
+get "https://getbin.io/FiloSottile/age?os=darwin&arch=arm64" "age/age-keygen" "Devantler.AgeCLI/runtimes/osx-arm64/native" "age-keygen-osx-arm64" "tar"
+get "https://getbin.io/FiloSottile/age?os=linux&arch=amd64" "age/age-keygen" "Devantler.AgeCLI/runtimes/linux-x64/native" "age-keygen-linux-x64" "tar"
+get "https://getbin.io/FiloSottile/age?os=linux&arch=arm64" "age/age-keygen" "Devantler.AgeCLI/runtimes/linux-arm64/native" "age-keygen-linux-arm64" "tar"
+get "https://getbin.io/FiloSottile/age?os=windows&arch=amd64" "age/age-keygen.exe" "Devantler.AgeCLI/runtimes/win-x64/native" "age-keygen-win-x64.exe" "zip"
 rm -rf Devantler.AgeCLI/runtimes/*/native/age
